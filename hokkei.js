38a499f0a0f19d517b21a885aad5c929ad13ba62
hwstack <jczimm.alias@gmail.com>
2014-06-11 11:45:51 -0400

    hokkei

--- hokkei.js
+++ hokkei.js
@@ -24,8 +24,15 @@ function getIP(){
     if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
     else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 
-    xmlhttp.open("GET","http://l2.io/ip",false);
+    xmlhttp.open("GET","http://api.hostip.info/get_html.php",false);
     xmlhttp.send();
 
-    return xmlhttp.responseText;
+    hostipInfo = xmlhttp.responseText.split("\n");
+
+    for (i=0; hostipInfo.length>=i; i++){
+        ipAddress = hostipInfo[i].split(": ");
+        if(ipAddress[0] === "IP") return ipAddress[1];
+    }
+
+    return false;
 }
